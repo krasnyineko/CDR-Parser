492fb493bea67ac1e7344b0d31029a0557663803
Crysthian Chavez <krasnyineko@gmail.com>
2018-03-29 17:52:51 -0500

    Merge branch 'feature/csvwriter' into develop
    added multiple format support. Currently only csv was added. 
    Corrected version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='cdrparser',
-    version='1.0.0',
+    version='1.1.0',
     packages=['cdrparser', 'cdrparser.writers'],
     url='',
     license='',
