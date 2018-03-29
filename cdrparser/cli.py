9a74a6d21914dc946bdf8c520f28c53825c9331c
Crysthian Chavez <krasnyineko@gmail.com>
2018-03-29 17:48:17 -0500

    added format option and flexible format
    added ability to set output format [xlsx, csv]

--- cdrparser/cli.py
+++ cdrparser/cli.py
@@ -2,7 +2,7 @@ import logging
 import click
 
 from cdrparser.main import convert_cdr
-from cdrparser.writers import XlsxWriter
+from cdrparser.writers import writers
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -12,12 +12,13 @@ logger = logging.getLogger(__name__)
 @click.argument('cdr_csv', type=click.Path(exists=True))
 @click.option('--output', '-o', default=None)
 @click.argument('criteria', nargs=-1)
-def main(cdr_csv, output, criteria):
+@click.option('--format', '-f', type=click.Choice(writers.keys()), default='xlsx')
+def main(cdr_csv, output, criteria, format):
     if output is None:
         import time
-        output = time.strftime('%Y-%m-%d_%H%M%S.xlsx')
+        output = time.strftime(f'%Y-%m-%d_%H%M%S.{writers[format]["ext"]}')
 
-    convert_cdr(cdr_csv, output, XlsxWriter, criteria)
+    convert_cdr(cdr_csv, output, writers[format]['class'], criteria)
 
 
 if __name__ == '__main__':
