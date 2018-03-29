56e8709a790434bd23ad7805be90b7e9238a79a3
Crysthian Chavez <krasnyineko@gmail.com>
2018-03-29 17:48:55 -0500

    added writers dict
    writers dict was added to support multiple formats

--- cdrparser/writers/__init__.py
+++ cdrparser/writers/__init__.py
@@ -1,3 +1,14 @@
 from .writer import Writer
 from .xlsxwriter import XlsxWriter
+from .csvwriter import CsvWriter
 
+writers = {
+    'xlsx': {
+        'ext': 'xlsx',
+        'class': XlsxWriter
+    },
+    'csv': {
+        'ext': 'csv',
+        'class': CsvWriter
+    }
+}
